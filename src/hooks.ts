70b913c2b23dc54a420e4053dd84068900ea9fbb
Anwesh43 <anweshthecool0@gmail.com>
2022-06-20 17:55:02 +0530

    adding useStyle hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -2,9 +2,9 @@
 const scGap : number = 0.01 
 const delay : number = 20 
 
-import {useState} from 'react'
+import {useState, useEffect, CSSProperties} from 'react'
 
-export const useAnimatScale = () => {
+export const useAnimatedScale = () => {
     const [scale, setScale] = useState(0)
     const [animated, setAnimated] = useState(false)
     const [minDeg, setMinDeg] = useState(0)
@@ -26,5 +26,77 @@ export const useAnimatScale = () => {
                 }, delay)
             }
         }
+    }
+}
+
+export const useDimension = () => {
+    const [w, setW] = useState(window.innerWidth)
+    const [h, setH] = useState(window.innerHeight)
+    useEffect(() => {
+        window.onresize = () => {
+            setW(window.innerWidth)
+            setH(window.innerHeight)
+        }
+        return () => {
+            window.onresize = () => {
+
+            }
+        }
+    }, [])
+    return {
+        w, 
+        h
+    }
+}
+
+export const useStyle = (w : number, h : number, scale : number) => {
+    const position = 'absolute'
+    const left = `${w / 2}px`
+    const top = `${h / 2}px`
+    const color = 'indigo'
+    const circleSize : number = Math.min(w, h) / 5 
+    const minuteSize : number = Math.min(w, h) / 16 
+    const hourSize : number = Math.min(w, h) / 25 
+    return {
+        parentStyle() : CSSProperties {
+            return {
+                position,
+                left, 
+                top, 
+            }
+        },
+        circleStyle() : CSSProperties {
+            return {
+                borderRadius: '50%',
+                border: `1px solid ${color}`,
+                width: `${circleSize}px`,
+                height: `${circleSize}px`,
+                left: `${-circleSize / 2}px`,
+                top: `${-circleSize / 2}px`
+            }
+        },
+        minuteHandStyle() : CSSProperties {
+            const width = Math.min(w, h) / 90
+            return {
+                height: `${minuteSize}px`,
+                width: `${width}px`,
+                background: color,
+                top: `${-minuteSize}px`,
+                left: `${-width / 2}px` 
+            }
+        },
+        hourHandStyle() : CSSProperties {
+            const width = Math.min(w, h) / 70
+            return {
+                height: `${hourSize}px`,
+                width: `${width}px`,
+                background: color,
+                top: `${-hourSize}px`,
+                left: `${-width / 2}px` 
+            }
+        },
+
+
+
     }
 }
